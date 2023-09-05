d788ccddbdb5d2f801abe0c9b03ceaa5bc1f31e1
Hans Pagel <mail@hanspagel.com>
2023-09-05 16:47:14 +0200

    refactor: use the cdn version for the cli

--- packages/cli/src/utils/render.ts
+++ packages/cli/src/utils/render.ts
@@ -1,27 +1,20 @@
-import { ApiReference } from '@scalar/api-reference'
-import { createSSRApp } from 'vue'
-// Vue's server-rendering API is exposed under `vue/server-renderer`.
-import { renderToString } from 'vue/server-renderer'
-
 export const render = (spec: string): Promise<string> =>
   new Promise((resolve) => {
-    const app = createSSRApp({
-      data: () => ({ spec }),
-      components: { ApiReference },
-      template: `<ApiReference :spec="spec" />`,
-    })
-
-    renderToString(app).then((html: string) => {
-      resolve(`<!DOCTYPE html>
+    resolve(`
+<!DOCTYPE html>
 <html>
   <head>
     <title>API Reference</title>
     <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1" />
   </head>
   <body>
-    ${html}
+    <!-- Add your own OpenAPI/Swagger spec file URL here: -->
+    <div data-spec='${JSON.stringify(spec)}' />
+    <script src="https://cdn.scalar.com/api-reference.standalone.js"></script>
   </body>
-</html>`)
-    })
+</html>
+`)
   })
