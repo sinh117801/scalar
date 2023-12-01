f4d2cbcafb78da86d82cc50d2a5fc9e9f3b2017c
Hans Pagel <mail@hanspagel.com>
2023-12-01 11:26:12 +0100

    chore: add super basic api reference route (wip)

--- examples/nest/src/app.service.ts
+++ examples/nest/src/app.service.ts
@@ -3,6 +3,28 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return `<!doctype html>
+<html>
+  <head>
+    <title>NestJS API Reference</title>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1" />
+    <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🦁</text></svg>">
+    <style>
+      body {
+        margin: 0;
+      }
+    </style>
+  </head>
+  <body>
+    <script
+      id="api-reference"
+      data-url="/api-json"
+      data-proxy-url="https://api.scalar.com/request-proxy"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@scalar/api-reference"></script>
+  </body>
+</html>`;
   }
 }
